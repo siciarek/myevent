6e0b58a3b28071a528a45f29a10562af6104f1f8
siciarek <siciarek@gmail.com>
2012-11-24 17:41:21 +0100

    initial commit

---
+++ src/MyApp/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace MyApp\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="_homepage")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/public.html", name="_public")
+     * @Template()
+     */
+    public function publicAction()
+    {
+        return array("title" => "Public page");
+    }
+
+    /**
+     * @Route("/secured.html", name="_secured")
+     * @Template()
+     */
+    public function securedAction()
+    {
+        return array("title" => "Secured page");
+    }
+
+    /**
+     * @Route("/editor.html", name="_editor")
+     * @Template()
+     */
+    public function editorAction()
+    {
+        return array("title" => "Editor");
+    }
+
+    public function getTokenAction()
+    {
+        return new Response($this->container->get('form.csrf_provider')
+            ->generateCsrfToken('authenticate'));
+    }
+}
---
+++ src/MyApp/UserBundle/Controller/WebServiceController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace MyApp\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\Query;
+
+use MyApp\UserBundle\Entity as E;
+use MyApp\UserBundle\Logic\Encoder;
+
+
+class WebServiceController extends Controller
+{
+    private $frames = array();
+
+    public function emailAction()
+    {
+        $frame = array();
+        $data = array();
+
+        try {
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Hello Email')
+                ->setFrom('siciarek@gmail.com')
+                ->setTo('j.siciarek@sescom.pl')
+                ->setBody('My HTML body', 'text/html')
+                ->addPart('My amazing body in plain text', 'text/plain');
+
+            $data[] = $this->get('mailer')->send($message);
+
+            $frame = $this->frames["ok"];
+            $frame["data"] = $data;
+        } catch (\Exception $e) {
+            $frame = $this->frames["error"];
+            $frame["msg"] = $e->getMessage();
+            $frame["data"]["errno"] = $e->getCode();
+        }
+
+        $json = json_encode($frame);
+        return new Response($json);
+    }
+
+    public function serviceAction()
+    {
+        $frame = array();
+        $data = array();
+
+        try {
+            $data = array(1, 2, Encoder::encode("helloworld"));
+
+            $frame = $this->frames["ok"];
+            $frame["data"] = $data;
+        } catch (\Exception $e) {
+            $frame = $this->frames["error"];
+            $frame["msg"] = $e->getMessage();
+            $frame["data"]["errno"] = $e->getCode();
+        }
+
+        $json = json_encode($frame);
+        return new Response($json);
+    }
+
+    public function failureAction()
+    {
+        $frame = array();
+        $data = array();
+
+        try {
+
+            throw new \Exception("Test error", 666);
+
+            $frame = $this->frames["ok"];
+            $frame["data"] = $data;
+        } catch (\Exception $e) {
+            $frame = $this->frames["error"];
+            $frame["msg"] = $e->getMessage();
+            $frame["data"]["errno"] = $e->getCode();
+        }
+
+        $json = json_encode($frame);
+        return new Response($json);
+    }
+
+    public
+    function preExecute()
+    {
+        $this->frames["ok"] = array(
+            "success"  => true,
+            "type"     => "info",
+            "datetime" => date("Y-m-d H:i:s"),
+            "msg"      => "OK",
+            "data"     => array(),
+        );
+
+        $this->frames["error"] = array(
+            "success"  => true,
+            "type"     => "error",
+            "datetime" => date("Y-m-d H:i:s"),
+            "msg"      => "Unexpected Exception",
+            "data"     => array(),
+        );
+    }
+}
